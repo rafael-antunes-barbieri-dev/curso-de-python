c22b14ed037cd73637128a37242f990a93537794
Rafael Antunes Barbieri <rafaelantunesbarbieri15@gmail.com>
2026-06-11 15:06:02 -0300

    feat(imc): Adiciona lógica para o cálculo de IMC

---
+++ 1_basico/exercicios/ex_calculo_do_imc.py
@@ -0,0 +1,15 @@
+nome = "Rafael Antunes"
+altura = 1.65
+peso = 45.2
+imc = ...
+
+"""
+IMC =    peso(em kg)
+      ___________________
+
+      altura ao quadrado
+"""
+
+imc = peso / altura ** 2
+
+print("Seu nome é" + " " + nome + " " + "e seu IMC é:" + " " + str(imc))
