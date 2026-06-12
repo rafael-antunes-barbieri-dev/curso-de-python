25e71c68ab92657ff5638e4a659f8ad538c38252
Rafael Antunes Barbieri <rafaelantunesbarbieri15@gmail.com>
2026-06-12 15:36:55 -0300

    feat: Adiciona exercicio sobre comparações

---
+++ 1_basico/exercicios/ex_comparacao.py
@@ -0,0 +1,7 @@
+primeiro_valor = int(input("Digite um numero: "))
+segundo_valor = int(input("Digite um segundo número: "))
+
+if primeiro_valor > segundo_valor:
+    print(f"\nO primeiro valor: {primeiro_valor} é maior que o segundo valor: {segundo_valor}")
+else:
+    print(f"O segundo valor: {segundo_valor} é maior do que o primeiro valor: {primeiro_valor}")
