31325bb3067371fffcd473a9d94047342ebacc96
Rafael Antunes Barbieri <rafaelantunesbarbieri15@gmail.com>
2026-06-18 21:46:41 -0300

    feat: Adiciona aula de f-strings de forma avançada

---
+++ 1_basico/advanced_fstrings.py
@@ -0,0 +1,5 @@
+letras = "ABC"
+
+print(f"{letras}")  # Imprime ABC sem espaços
+print(f"{letras: >10}")  # Imprime ABC com 10 espaços para direita
+print(f"{letras: <10}")  # Imprime ABC com 10 espaços para esquerda
