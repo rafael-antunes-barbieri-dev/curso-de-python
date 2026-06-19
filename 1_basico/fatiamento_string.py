b8a843ad5ea7356e5e4cf5a43297a05666fa8476
Rafael Antunes Barbieri <rafaelantunesbarbieri15@gmail.com>
2026-06-18 22:00:04 -0300

    feat: Adiciona aula sobre fatiamento de strings

---
+++ 1_basico/fatiamento_string.py
@@ -0,0 +1,11 @@
+mensagem = "Olá mundo"
+print(mensagem[5])
+print(mensagem[-4])
+
+# Fatiamento
+# [i:f]
+print(mensagem[4:])  # Se não colocar nd no fim imprime todo o resto
+print(mensagem[4:8])  # Determina onde vai parar
+
+# A função len
+print(len(mensagem))  # Mostra quantos caracteres minha variavel tem (incluindo espaços)
