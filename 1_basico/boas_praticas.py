3d00a38c9ae9734ba94694c30695d10009708937
Rafael Antunes Barbieri <rafaelantunesbarbieri15@gmail.com>
2026-06-20 16:38:33 -0300

    feat: Adiciona aula de boas práticas em python

---
+++ 1_basico/boas_praticas.py
@@ -0,0 +1,8 @@
+"""
+Não existe constantes no python
+mas existe uma convenção:
+se escreve a variável com letras maiusculas
+para indicar que o valor não se deve mudar
+"""
+
+# Muitas condições dentro de um if (ruim)
